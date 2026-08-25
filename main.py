b3d5a1d84483a1663435d0bc5ce95f4dbb4ad4e1
Cauã Mendes Correia Neto <cmcn1@poli.br>
2026-08-25 14:45:20 -0300

    Primeiro commit: lógica básica da vazão do tanque

---
+++ main.py
@@ -0,0 +1,5 @@
+# Criar uma classe Tanque em Python. Ela deve ter uma taxa de vazamento constante e um método para receber água da válvula.
+
+print("digite um numero")
+numero = (input())
+print(numero)
