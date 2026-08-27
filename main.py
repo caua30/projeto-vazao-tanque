33b41c3ddbc33ec20a3fc1225df106a22afe86cf
Cauã Mendes Correia Neto <cmcn1@poli.br>
2026-08-27 17:58:04 -0300

    classe criada e logica iniciada

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Criar uma classe Tanque em Python. Ela deve ter uma taxa de vazamento constante e um método para receber água da válvula.
 
-print("digite um numero")
-numero = (input())
-print(numero)
+class Tanque:
+    def __init__(self, capacidade_agua):
+        self.capacidade_atual = capacidade_agua
+        self.capacidade_maxima = 10000
+
+    def receber_agua(self, quantidade_recebida):
+        self.capacidade_atual += quantidade_recebida
+
+    def taxa_vazamento(self,pressão_final , pressão_inicial, volume, tempo):
+        taxa_de_vazamento = (pressão_final - pressão_inicial)*volume/tempo
+        self_capacidade_atual - self.taxa_vazamento == self.capacidade_atual
+
+
